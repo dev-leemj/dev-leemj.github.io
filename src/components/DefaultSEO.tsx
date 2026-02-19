6d733f14b7c5cb3dbfb06a724eeffef688bd7016
root <root@localhost.localdomain>
2026-02-19 11:57:04 +0900

    [DEL] remove old files

--- src/components/DefaultSEO.tsx
+++ src/components/DefaultSEO.tsx
@@ -1,15 +1,15 @@
 import { DefaultSeo, DefaultSeoProps } from "next-seo";
 
 const DEFAULT_SEO: DefaultSeoProps = {
-	title: "MitNy.log",
+	title: "Dev - Lee Mijin",
 	description: "기록 저장소",
-	canonical: "https://mitny.github.io",
+	canonical: "https://dev-leemj.github.io",
 	openGraph: {
 		type: "website",
 		locale: "ko_KR",
-		url: "https://mitny.github.io",
-		title: "MitNy.log",
-		site_name: "MitNy.log",
+		url: "https://dev-leemj.github.io",
+		title: "Dev - Lee Mijin",
+		site_name: "Dev - Lee Mijin",
 		description: "기록 저장소"
 	}
 };
